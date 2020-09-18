3c7f6bf6b6108cc41547a0cce7748a85e40e5bad
Somesh Koli <kolisomesh27@gmail.com>
2020-09-18 07:53:33 +0530

    implementss connection read write methods and completes initialrequest

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -11,11 +11,43 @@ import (
 	"github.com/someshkoli/tunnel/pkg/schema"
 )
 
-func register(conn net.Conn) {
-	// TODO do registration processs herre
+func send(conn net.Conn, res *schema.Response) {
+	messageBuffer := new(bytes.Buffer)
+	gob.NewEncoder(messageBuffer).Encode(res)
+	conn.Write(messageBuffer.Bytes())
+	fmt.Println("messageSent")
 }
 
-func makeLocalRequest(request schema.Request) (*http.Response, error) {
+func recieve(conn net.Conn) (*schema.Request, error) {
+	requestBuffer := make([]byte, schema.MaxDataSize)
+	_, err := conn.Read(requestBuffer)
+	if err != nil {
+		fmt.Println(err)
+		return &schema.Request{}, err
+	}
+	bufferRequestCollector := bytes.NewBuffer(requestBuffer)
+	request := new(schema.Request)
+	gob.NewDecoder(bufferRequestCollector).Decode(request)
+	return request, nil
+}
+
+func register(conn net.Conn) error {
+	// TODO Recieveing initial data
+	fmt.Println("Recieveing initial data")
+	request, err := recieve(conn)
+	if err != nil {
+		return err
+	}
+	// sending initial data
+	fmt.Println("registered")
+
+	response := schema.MakeResponse(schema.Initializing, request.ID, request.Host, request.Port, http.Response{})
+	send(conn, &response)
+	fmt.Println(request.ID)
+	return nil
+}
+
+func makeLocalRequest(request *schema.Request) (*http.Response, error) {
 	client := http.Client{}
 	// replace hostname and port and then send request
 	res, err := client.Do(&request.Request)
@@ -24,57 +56,52 @@ func makeLocalRequest(request schema.Request) (*http.Response, error) {
 
 // Client - Client for tunnel connection
 type Client struct {
-	ID    string
-	Lport int    `json:"Lport"`
-	Rport int    `json:"Rport"`
-	Lhost string `json:"Lhost"`
-	Rhost string `json:"Rhost"`
+	ID   string
+	host string `json:"host"`
+	port int    `json:"host"`
 }
 
 // NewClient - Returns instance of new client
-func NewClient(Lhost string, Lport int, Rhost string, Rport int) *Client {
+func NewClient(host string, port int) *Client {
 	client := Client{
-		Lhost: Lhost,
-		Lport: Lport,
-		Rhost: Rhost,
-		Rport: Rport,
+		host: host,
+		port: port,
 	}
 	return &client
 }
 
 // ConnectAndListen - connect to the host server
 func (client *Client) ConnectAndListen() {
-	conn, err := net.Dial("tcp", "host:"+strconv.Itoa(client.Rport))
+	var done chan bool
+	conn, err := net.Dial("tcp", client.host+":"+strconv.Itoa(client.port))
 	if err != nil {
-		fmt.Println("Couldnot connect to the server")
+		fmt.Println(err)
+		return
 	}
 	// registers the localtunnel
 	register(conn)
 
 	// handling local tunnel requests
-	go handleConnection(conn)
+	go handleConnection(conn, done)
+	<-done
 }
 
-func handleConnection(conn net.Conn) {
+func handleConnection(conn net.Conn, done chan bool) {
 	defer conn.Close()
 	for {
 		// read incomming request
-		requestBuffer := make([]byte, schema.MaxDataSize)
-		_, err := conn.Read(requestBuffer)
+		request, err := recieve(conn)
 		if err != nil {
-			break
+			fmt.Println(err)
+			done <- true
 		}
-		bufferRequestCollector := bytes.NewBuffer(requestBuffer)
-		request := new(schema.Request)
-		gob.NewDecoder(bufferRequestCollector).Decode(request)
 
 		// calling local client
-		res, err := makeLocalRequest(Request)
+		res, err := makeLocalRequest(request)
 
 		// writing response back to the host
-		response := schema.MakeResponse(schema.Connected, request.ID, request.Rhost, request.Rport, request.Lhost, request.Lport, *res)
-		messageBuffer := new(bytes.Buffer)
-		gob.NewEncoder(messageBuffer).Encode(response)
-		conn.Write(messageBuffer.Bytes())
+		response := schema.MakeResponse(schema.Connected, request.ID, request.Host, request.Port, *res)
+		send(conn, &response)
 	}
+	done <- true
 }
